9b78602951a0693ba26893a698495d421b21ff00
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-25 16:05:52 -0800

    working language toggle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,57 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+
+const languages = [
+  {
+    code: 'fr',
+    name: 'Français',
+    country_code: 'fr',
+  },
+  {
+    code: 'en',
+    name: 'English',
+    country_code: 'gb',
+  },
+  {
+    code: 'es',
+    name: 'Español',
+    country_code: 'es'
+  }
+]
+
+
 function App() {
+  const { t } = useTranslation()
+  const releaseDate = new Date('2024-02-25')
+  const timeDiffernce= new Date() - releaseDate
+  const number_of_days = Math.floor(timeDiffernce / (1000 * 60 * 60 *24))
   return (
-    <div className="App">
-      Hello World
+    <div className="container">
+      <div className="d-Flex justify-content-end">
+      <div className="dropdown">
+  <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Change language
+  </button>
+  <ul className="dropdown-menu">
+    {languages.map(({ code, name, country_code}) => (
+      <li key={country_code}>
+        <button className="dropdown-item" onClick= {() => i18next.changeLanguage(code)}>
+          <span className={`flag-icon flag-icon-${country_code} mx-2`}>
+
+          </span>
+          {name}
+          </button>
+      </li>
+    ))}
+    
+  </ul>
+</div> 
+      </div>
+      <div clasName= "d-flex flex-column align-itmes-start">
+        <h1 className="font-weight-normal mb-3">{t('welcome_message')}</h1>
+        <p>{t('days_since_release', {number_of_days })}</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.js'
+// import 'flag-icon-css/css/flag-icon.min.css'
+
 import App from './App';
 
+i18n
+  .use(initReactI18next) 
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en','fr','es'],
+    fallbackLng: "en",
+    detection: {
+      order: ['htmlTag','cookie', 'localStorage', 'path', 'subdomain'],
+      cashes: ['cookie']
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    // react: {useSuspense: false}
+  })
+
+  const loadingMarkup = (
+    <div className='py-4 text-center'>
+      <h2>Loading..</h2>
+    </div>
+  )
+
+
+
 ReactDOM.render(
+  <Suspense fallback ={loadingMarkup}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Suspense>,
   document.getElementById('root')
 );
